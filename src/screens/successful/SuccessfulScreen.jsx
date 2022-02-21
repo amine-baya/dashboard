1e03945d7f81e59ae3af74fdf7d521d93105d56c
--unset <abaya6054@gmail.com>
2022-02-21 18:06:50 +0100

    create successful page

---
+++ src/screens/successful/SuccessfulScreen.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import LargeButton from '../../components/largeButton/LargeButton'
+import './successful.css'
+
+const SuccessfulScreen = () => {
+  return (
+    <div className='container'>
+      <div id='successful'>
+        <img src="images/success.png" alt="success" />
+        <h3>Submission successful!</h3>
+        <p>Thanks! We have received your submission. Our Consultant will contant you in a while</p>
+        <LargeButton text="Continue" />
+      </div>
+
+
+    </div>
+  )
+}
+
+export default SuccessfulScreen
