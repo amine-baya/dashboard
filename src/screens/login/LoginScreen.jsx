f5692d675ef0595dc94eff32d3f86135522402ab
--unset <abaya6054@gmail.com>
2022-03-24 16:50:04 +0100

    complete navigation except about-me

--- src/screens/login/LoginScreen.jsx
+++ src/screens/login/LoginScreen.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import React, { useState,useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import Header from '../../components/header/Header'
-import Input from '../../components/input/Input'
-import LargeButton from '../../components/largeButton/LargeButton'
+import { UserInfo } from '../../helpers/ContextApi'
+
+
 import Title from '../../components/title/Title'
 import './login.css'
 
 const LoginScreen = () => {
+  const {setUserInfo} = useContext(UserInfo)
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
   let navigate = useNavigate()
+
+  const submitHandler = async(e)=>{
+    e.preventDefault()
+    
+    const config = {
+      headers: {
+          'Content-Type': 'application/json',
+      },
+    }
+
+    await axios.post('https://toptal.ibrcloud.com/api/v1/auth/login', {email, password}, config).then(res =>{
+      setUserInfo(res.data)
+      localStorage.setItem("userInfo", JSON.stringify(res.data) )
+      navigate('/personal-information')
+    }).catch(err =>{
+      console.log(err);
+    })
+    
+}
+
 
   return (
     <>
@@ -16,14 +41,27 @@ const LoginScreen = () => {
     <div id='login'>
         <h6>Welcome back</h6>
         <Title title="Login to your account" />
-        <Input label="Email Address" placeH="Type your e-mail " type="email" />
-        <Input label="Password" placeH="Enter Password" type="password" />
-        <div className='password_memory'>
-            <div><input type="radio"></input> Remember me</div>
-            <div><span style={{color:"#2B6CB0"}} onClick={() => navigate('/verification-password')}>Forgot password?</span></div>
-        </div>
-        <LargeButton text="Log in" />
-        <p className='resend_verification'>Dont have an account? <span>Join free today </span></p>
+
+        <form onSubmit={submitHandler}> 
+            <div className='input_component'>
+              <label className='label'>Email Address</label>
+              <input type="email" placeholder="Type your e-mail " onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div className='input_component'>
+              <label className='label'>Password</label>
+              <input type="password" placeholder="Enter Password" onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <div className='password_memory'>
+                <div><input type="radio"></input> Remember me</div>
+                <div><span style={{color:"#2B6CB0"}}> <Link to="/verification-password">Forgot password?</Link> </span></div>
+            </div>
+            
+            <button className='large_button' type='submit'  >
+                Log in
+            </button>
+
+        </form>
+        <p className='resend_verification'>Dont have an account? <span> <Link to="/register" >Join free today</Link>  </span></p>
     </div>
     </div>
     </>
