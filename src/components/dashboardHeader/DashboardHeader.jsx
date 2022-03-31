71019100ebe99fa24b3fbbbc623b3cb225d9a025
--unset <abaya6054@gmail.com>
2022-03-31 11:08:46 +0100

    fetch the real user info

--- src/components/dashboardHeader/DashboardHeader.jsx
+++ src/components/dashboardHeader/DashboardHeader.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import Input from '../input/Input'
+import React, { useContext } from 'react'
+import { UserInfo } from '../../helpers/ContextApi'
 import './dashboardHeader.css'
 
 
 const DashboardHeader = () => {
+  const {personalData} = useContext(UserInfo)
+  console.log(personalData);
+
   return (
     <>
     <div className='container_dashboard_right'>
@@ -17,7 +20,7 @@ const DashboardHeader = () => {
             <div className="Dashboard_Header_notification">
               <ul>
                 <li><img src='../../images/notification.png' alt='notification' /></li>
-                <li><img src='../../images/profile_img_header.png' alt='profile' /> <span>Ashley H.</span></li>
+                <li className='Dashboard_Header_profile_image'><img src={personalData?.profile }  alt='profile' /> <span>{personalData?.first_name}</span></li>
                 <li><img src='../../images/drop_down_header.png' alt='drop down ' /></li> 
               </ul> 
             </div>
