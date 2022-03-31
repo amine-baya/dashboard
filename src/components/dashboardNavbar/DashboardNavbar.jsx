50dc0a2658b35496d3a7010e99cc0c9be04e99ce
--unset <abaya6054@gmail.com>
2022-03-31 12:12:57 +0100

    add logout to navbar dashboard

--- src/components/dashboardNavbar/DashboardNavbar.jsx
+++ src/components/dashboardNavbar/DashboardNavbar.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import './dashboardNavbar.css'
 import {ImProfile} from 'react-icons/im'
+import {AiOutlineLogout} from 'react-icons/ai'
 
 const DashboardNavbar = () => {
   let navigate = useNavigate()
+
+  const logout =()=>{
+    localStorage.removeItem("userInfo")
+    localStorage.removeItem("personalData")
+    navigate("/")
+  }
   return (
     <div id='dashboard_navbar'>
       <div className='dashboard_navbar_container'>
@@ -18,6 +25,7 @@ const DashboardNavbar = () => {
               <li><img src='../../images/profile.png' alt='Profile' /> <span>Profile</span></li>
               <li><img src='../../images/candidate.png' alt='Candidates' /> <span>Candidates</span></li> 
               <li onClick={()=> navigate("/resume")}><ImProfile color="#A3AED0" size="1.7em" /> <span style={{paddingLeft:"20px"}}>resume</span></li> 
+              <li onClick={()=> logout()}><AiOutlineLogout color="#A3AED0" size="1.7em" /> <span style={{paddingLeft:"20px"}}>logOut</span></li> 
             </ul> 
         </div>
 
