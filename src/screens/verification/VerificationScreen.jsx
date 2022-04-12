e3f77a75301a99981b7c4dd488200843f19ef687
--unset <abaya6054@gmail.com>
2022-04-12 07:45:25 +0100

    make functionality of verification email using the api

--- src/screens/verification/VerificationScreen.jsx
+++ src/screens/verification/VerificationScreen.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../../components/header/Header'
 import LargeButton from '../../components/largeButton/LargeButton'
+import { UserInfo } from '../../helpers/ContextApi'
 import './verification.css'
 
 const VerificationScreen = () => {
+
+  const [digit1,setDigit1] = useState('')
+  const [digit2,setDigit2] = useState('')
+  const [digit3,setDigit3] = useState('')
+  const [digit4,setDigit4] = useState('')
+  const {userInfo, setVerifyEmailModalShow, setVerifiedEmailModalShow} = useContext(UserInfo)
+  let navigate = useNavigate()
+
+
+
+
+
+  const verfyEmailCode =()=>{
+
+    const code = digit1 + digit2 + digit3 + digit4
+   
+    const config = {
+      headers: {
+        Authorization:`Bearer ${userInfo?.token}`
+      },
+    }
+    
+    axios.post('https://toptal.ibrcloud.com/api/v1/user/otp-verify',{otp: code.toString()}, config).then(res =>{
+      setVerifiedEmailModalShow(true)
+      setVerifyEmailModalShow("false")
+    navigate("/calender");
+    
+  }).catch(err =>{
+      console.log(err.response);
+  })
+
+  }
+
+
+
+
+    const verifyEmail =()=>{
+
+      const config = {
+        headers: {
+          Authorization:`Bearer ${userInfo?.token}`
+        },
+      }
+
+    axios.post('https://toptal.ibrcloud.com/api/v1/user/otp-send-mail', {}, config).then(res =>{
+
+      
+      console.log(res);
+      
+    }).catch(err =>{
+        console.log(err.response);
+    })
+    }
   return (
     <>
     <Header /> 
@@ -13,17 +69,22 @@ const VerificationScreen = () => {
           <div className='mail_outline_verification'>
             <img src="../../images/verify.png" alt="verify" className='mail_outline_verification_verify' />
             <img src="../../images/mail-outline.png" alt="mail-outline" className='mail_outline_verification_outline' />
+          </div>
+          <p>Enter 4 didgit verification code we sent at <span>{userInfo?.user.email}</span></p>
+          <div className='code_verification'>
+            <input type="text"   maxLength="1"  value={digit1} onChange={(e)=> setDigit1(e.target.value.replace(/\D/g,''))} />
+            <input  type="text"  maxLength="1"  value={digit2} onChange={(e)=> setDigit2(e.target.value.replace(/\D/g,''))} />
+            <input  type="text"  maxLength="1"  value={digit3} onChange={(e)=> setDigit3(e.target.value.replace(/\D/g,''))} />
+            <input  type="text"  maxLength="1"  value={digit4} onChange={(e)=> setDigit4(e.target.value.replace(/\D/g,''))} />
             
           </div>
-          <p>Enter 4 didgit verification code we sent at <span>tonikjone@demoo.com</span></p>
-          <div className='code_verification'>
-            <input type="number" max={1} min={1} maxlength={1} size={1} />
-            <input type="text" />
-            <input type="text" />
-            <input type="text" />
+         
+          <div >
+              <button className='large_button' onClick={()=>verfyEmailCode()} >
+                Submit
+              </button>
           </div>
-          <LargeButton text="Submit" nav='/create-password'/>
-          <p className='resend_verification'>Didn’t get  the code? <span  >Resend</span></p>
+          <p className='resend_verification'>Didn’t get  the code? <span onClick={()=>verifyEmail()}  >Resend</span></p>
         </div>
       </div>
     </>
