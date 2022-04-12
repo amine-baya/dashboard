146ccdbc69e2453ac97d8abca6b8f9dbf3c74fd0
--unset <abaya6054@gmail.com>
2022-04-12 07:46:28 +0100

    make functionality of verification password  using the api

--- src/screens/verificationPassword/VerificationPasswordScreen.jsx
+++ src/screens/verificationPassword/VerificationPasswordScreen.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
+import {useNavigate } from 'react-router-dom'
 import Header from '../../components/header/Header'
-import Input from '../../components/input/Input'
-import LargeButton from '../../components/largeButton/LargeButton'
 import './verificationPassword.css'
 
 const VerificationPasswordScreen = () => {
+  const [resetEmail,setResetEmail] = useState('')
+  let navigate = useNavigate()
+
+  const resetPassword =  ()=>{
+
+    axios.post('https://toptal.ibrcloud.com/api/v1/auth/forget-password', {email:resetEmail}).then(res =>{
+    
+    console.log("reset password successfuly");
+    navigate('/login')
+    
+}).catch(err =>{
+  console.log(err.response.data);
+})
+
+  }
   return (
     <>
     <Header /> 
@@ -16,8 +31,16 @@ const VerificationPasswordScreen = () => {
         <img src="../../images/mail-outline.png" alt="mail-outline" className='mail_outline_verification_outline' /> 
       </div>
       <p>Enter the email associated with your account</p>
-      <Input placeH="Enter email address" type="text" />
-      <LargeButton text="Resset Password" />
+    
+      <div className='input_component'>
+            <input type="email" placeholder="Enter email address" onChange={(e) => setResetEmail(e.target.value)}/>
+      </div>
+      
+      <div >
+        <button className='large_button' onClick={()=> resetPassword()}  >
+          Resset Password
+        </button>
+    </div>
       <p className='try_way'>Try another way</p>
     </div>
   </div>
